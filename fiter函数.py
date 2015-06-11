97a5c25119aaa85715f4961f5a5c0295d984f308
hanjiayu <13390934591@163.com>
2015-06-11 17:30:36 +0800

    fiter

---
+++ fiter函数.py
@@ -0,0 +1,11 @@
+def is_nod(n):
+    return n % 2 == 1
+
+L = range(100)
+
+print list(filter(is_nod,L))
+
+def not_empty(s):
+    return s and s.strip()
+
+print list(filter(not_empty,['A','','B',None,'C','  ']))
